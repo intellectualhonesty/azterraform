76ceab1773e87c243476d7a74ce86df6f3c06e1f
zjhe <hezijie@microsoft.com>
2022-04-26 14:33:58 +0800

    Upgrade attestation provider example's variables.tf

--- quickstart/101-attestation-provider/main.tf
+++ quickstart/101-attestation-provider/main.tf
@@ -1,10 +1,8 @@
 terraform {
-
   required_version = ">=0.12"
-    
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "~>2.0"
     }
   }
@@ -15,14 +13,14 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "rg" {
-  name = var.resource_group_name
+  name     = var.resource_group_name
   location = var.resource_group_location
 }
 
 resource "azurerm_attestation_provider" "corpAttestation" {
-    name                              = var.attestation_provider_name
-    resource_group_name               = azurerm_resource_group.rg.name
-    location                          = azurerm_resource_group.rg.location
+  name                = var.attestation_provider_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
 
-    policy_signing_certificate_data   = file(var.policy_file)
+  policy_signing_certificate_data = file(var.policy_file)
 }
--- quickstart/101-attestation-provider/variables.tf
+++ quickstart/101-attestation-provider/variables.tf
@@ -1,15 +1,23 @@
 variable "resource_group_name" {
-  default = "myResourceGroup"
+  type        = string
+  description = "The Name which should be used for this Resource Group. Changing this forces a new Resource Group to be created."
+  default     = "myResourceGroup"
 }
 
 variable "resource_group_location" {
-  default = "eastus"
+  type        = string
+  description = "The Azure Region where the Resource Group should exist. Changing this forces a new Resource Group to be created."
+  default     = "eastus"
 }
 
 variable "policy_file" {
-  default = "~/.certs/cert.pem"
+  type        = string
+  description = "File path to a valid X.509 certificate (Section 4 of RFC4648) file. Changing this forces a new resource to be created."
+  default     = "~/.certs/cert.pem"
 }
 
 variable "attestation_provider_name" {
-  default = "attestationprovider007"
+  type        = string
+  description = "The name which should be used for this Attestation Provider. Changing this forces a new resource to be created."
+  default     = "attestationprovider007"
 }
